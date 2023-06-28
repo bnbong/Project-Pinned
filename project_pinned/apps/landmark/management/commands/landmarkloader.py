821708d72562a06f0766c6c558cf33cc1daefe58
bnbong <bbbong9@gmail.com>
2023-06-28 16:15:10 +0900

    [PP-5, PP-13] 빅데이터에서 랜드마크를 불러와 Landmark table로 저장하는 기능 구현

---
+++ project_pinned/apps/landmark/management/commands/landmarkloader.py
@@ -0,0 +1,34 @@
+"""
+랜드마크 데이터가 담겨 있는 빅데이터를 통해 랜드마크 정보를 불러오고
+Landmark 테이블에 모두 저장하는 모듈.
+
+TODO: csv 1903번 데이터부터는 UTM-K표기법의 위/경도가 아니라 WGS84 표기법이 사용되었다.
+표기법을 통일시키는 로직을 구현하기.
+"""
+import pandas as pd
+
+from django.core.management import BaseCommand
+from apps.landmark.models import Landmark
+
+
+class Command(BaseCommand):
+    help = 'Load landmarks from csv into DB'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str)
+
+    def handle(self, *args, **options):
+        df = pd.read_csv(options['csv_file'])
+        df.drop_duplicates(subset='KLANG_NM', keep='first', inplace=True)
+        df.dropna(subset=['FCLTY_LO', 'FCLTY_LA'], inplace=True)
+
+        landmarks = [
+            Landmark(
+                name=row['KLANG_NM'],
+                location_lon=row['FCLTY_LO'],
+                location_lat=row['FCLTY_LA']
+            )
+            for _, row in df.iterrows()
+        ]
+
+        Landmark.objects.bulk_create(landmarks)
--- project_pinned/apps/landmark/models.py
+++ project_pinned/apps/landmark/models.py
@@ -3,7 +3,8 @@ from django.conf import settings
 
 
 class Landmark(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    location_lat = models.DecimalField(max_digits=9, decimal_places=6)
-    location_lon = models.DecimalField(max_digits=9, decimal_places=6)
-    image = models.ImageField(null=True, blank=True, upload_to=settings.MEDIA_ROOT)
+    location_lat = models.DecimalField(max_digits=20, decimal_places=10)
+    location_lon = models.DecimalField(max_digits=20, decimal_places=10)
+    image = models.ImageField(null=True, blank=True, upload_to='landmark_thumbnails/')
