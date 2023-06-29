3df1f071c4dc66572bf46dfd4d54f6e07919a50a
JJongmen <pjy90123@gmail.com>
2023-06-29 21:06:02 +0900

    [PP-4, PP-7] 게시글 읽기, 게시글 삭제 구현 및 게시글 생성하기 로직 구현중

--- project_pinned/apps/post/urls.py
+++ project_pinned/apps/post/urls.py
@@ -1,5 +1,10 @@
+from . import views
+
 from django.urls import path
 
 urlpatterns = [
-
+    path("", views.MakePost.as_view(), name="make-post"),
+    path("<int:post_id>/", views.PostCRUD.as_view(), name="post-crud"),
+    path("<str:user_id>/", views.PostsByUser.as_view(), name="posts-by-user"),
+    path("feed/", views.PostFeed.as_view(), name="post-feed"),
 ]
