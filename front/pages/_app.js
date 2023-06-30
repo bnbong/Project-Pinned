ebb750f505454bb920ad445c42d751ce6e21c1f5
suyong choi <dpfprtus@gmail.com>
2023-06-30 17:43:06 +0900

    [PP-20] login 페이지 초안

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,13 +1,19 @@
 import Layout from "@/components/Layout";
 import "./../styles/globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <AuthProvider>
-      <Layout>
+      {router.pathname == "/login" ? (
         <Component {...pageProps} />
-      </Layout>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </AuthProvider>
   );
 }
