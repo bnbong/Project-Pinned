840f85479e5e89d9d4678565d2fdb32dfb5b3969
bnbong <bbbong9@gmail.com>
2023-06-30 23:40:36 +0900

    [PP-1] CORS 설정 추가

--- project_pinned/project_pinned/settings.py
+++ project_pinned/project_pinned/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    'corsheaders.middleware.CorsMiddleware',  
 ]
 
 ROOT_URLCONF = "project_pinned.urls"
@@ -81,6 +83,11 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
+                         ,'http://localhost:3000'
+                         ,'http://0.0.0.0:3000']
+CORS_ALLOW_CREDENTIALS = True
 
 WSGI_APPLICATION = "project_pinned.wsgi.application"
 
