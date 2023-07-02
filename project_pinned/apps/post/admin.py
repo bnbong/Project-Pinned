7140b65f690e79c8109e69ffb0661bb5c77df717
bnbong <bbbong9@gmail.com>
2023-07-02 21:43:15 +0900

    [PP-3, PP-4, PP-5] admin 페이지 수정, user model 컬럼 추가-date_joined(원래 있던 db 인스턴스들은 현재 시간으로 date_joined가 설정됨), is_staff

--- project_pinned/apps/post/admin.py
+++ project_pinned/apps/post/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Image, Comment, Like
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'landmark', 'title', 'content', 'created_at', 'updated_at']
+    list_display_links = ['id', 'user', 'landmark', 'title', 'content', 'created_at', 'updated_at']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'image', 'created_at']
+    list_display_links = ['id', 'post', 'image', 'created_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'post', 'content', 'created_at', 'updated_at']
+    list_display_links = ['id', 'user', 'post', 'content', 'created_at', 'updated_at']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'post', 'created_at']
+    list_display_links = ['id', 'user', 'post', 'created_at']
--- project_pinned/apps/user/admin.py
+++ project_pinned/apps/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Follow
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'is_staff', 'is_active', 'date_joined']
+    list_display_links = ['id', 'username', 'email', 'is_staff', 'is_active', 'date_joined']
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ['id', 'follower', 'following']
+    list_display_links = ['id', 'follower', 'following']
