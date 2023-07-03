3f23cbbbf8b549d6a85f93e59e44d70bfd08c95c
suyong choi <dpfprtus@gmail.com>
2023-07-03 14:32:07 +0900

    [PP-20] 로그인/회원가입 페이지 연결

--- front/pages/login.js
+++ front/pages/login.js
@@ -1,4 +1,5 @@
 import Input from "@/components/Input";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Login() {
@@ -65,18 +66,18 @@ export default function Login() {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Sign in
+                  로그인하기
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   계정이 아직 없으신가요?{" "}
-                  <a
-                    href="#"
+                  <Link
+                    href="/signup"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
                     가입하기
-                  </a>
+                  </Link>
                 </p>
               </form>
             </div>
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Input from "../components/Input";
+import Link from "next/link";
 
 export default function Login() {
   const [inputs, setInputs] = useState({
@@ -135,18 +136,18 @@ export default function Login() {
               </div>
               <button
                 type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Create an account
+                회원가입하기
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 이미 계정이 있으신가요?{" "}
-                <a
-                  href="#"
+                <Link
+                  href="/login"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   로그인 하기
-                </a>
+                </Link>
               </p>
             </form>
           </div>
