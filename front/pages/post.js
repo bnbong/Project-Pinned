b51717b2ac95ba5b8042d226e4f4680625e25447
dpfprtus <dpfprtus@gmail.com>
2023-07-06 01:41:25 +0900

    convert to react-quill

--- front/pages/post.js
+++ front/pages/post.js
@@ -1,115 +1,116 @@
-import { React, useState, useCallback, useMemo } from "react";
+import dynamic from "next/dynamic";
+import { useRef, useState, useMemo } from "react";
+import Input from "@/components/Input";
+import "react-quill/dist/quill.snow.css";
 
-import "easymde/dist/easymde.min.css";
-import Input from "./../components/Input";
+export default function Post({ html, setHtml }) {
+  const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+  const quillRef = useRef(null);
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: "1" }, { header: "2" }],
+          [{ size: [] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }, { align: [] }],
+          ["image"],
+        ],
+        // handlers: { image: imageHandler },
+      },
+      clipboard: {
+        matchVisual: false,
+      },
+    }),
+    []
+  );
 
-export default function Post() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("initial content");
-  const [imageSrc, setImageSrc] = useState(null);
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "align",
+    "image",
+  ];
+  const onChange = () => {};
 
-  const onChange = (e) => {
-    const { id, value } = e.target;
-    if (id == "title") {
-      setTitle(value);
-    }
-    if (id == "content") {
-      setContent(value);
-    }
-  };
-
-  const onUpload = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    return new Promise((resolve) => {
-      reader.onload = () => {
-        setImageSrc(reader.result || null); // 파일의 컨텐츠
-        resolve();
-      };
-    });
-  };
   return (
-    <>
-      <div className="mt-10 mx-10 h-max">
-        <div className="mb-6">
-          <Input
-            name="제목"
-            id="title"
-            onChange={onChange}
-            placeholder="제목을 작성하세요"
-          />
-        </div>
-        <label
-          htmlFor="default-input"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          내용
-        </label>
-        <div className="r mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-          <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-            <textarea
-              id="content"
-              onChange={onChange}
-              rows="4"
-              className="w-full h-80 px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-              placeholder="내용을 작성하세요."
-              required
-            ></textarea>
-            <img width={"50%"} src={imageSrc} />
-          </div>
-          <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-            <button
-              type="submit"
-              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
-            >
-              작성하기
-            </button>
-            <div className="flex pl-0 space-x-1 sm:pl-2">
-              <input
-                style={{ height: "0px", overflow: "hidden" }} //사진 아이콘 클릭시 이미지 파일선택창 불러오기 구현
-                accept="image/*"
-                id="image_input"
-                multiple
-                type="file"
-                onChange={(e) => onUpload(e)}
-              />
-              <button
-                type="button"
-                onClick={() => document.getElementById("image_input").click()}
-                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 18"
-                >
-                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                </svg>
-                <span className="sr-only">Upload image</span>
-              </button>
-              <button
-                type="button"
-                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 20"
-                >
-                  <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
-                </svg>
-                <span className="sr-only">Set location</span>
-              </button>
-            </div>
-          </div>
-        </div>
+    <div className="mx-10 mt-10">
+      <div className="mb-6">
+        <Input
+          name="제목"
+          id="title"
+          onChange={onChange}
+          placeholder="제목을 작성하세요"
+        />
       </div>
-    </>
+      <div>
+        <ReactQuill
+          className="h-96"
+          ref={quillRef}
+          onChange={(e) => console.log(e)}
+          modules={modules}
+          formats={formats}
+          value={html}
+          placeholder={"내용을 작성해 주세요!"}
+          theme="snow"
+        />
+      </div>
+
+      <div className="flex pl-0 space-x-1 sm:pl-2 mt-20">
+        <button
+          type="submit"
+          className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+        >
+          작성하기
+        </button>
+        <input
+          style={{ height: "0px", overflow: "hidden" }} //사진 아이콘 클릭시 이미지 파일선택창 불러오기 구현
+          accept="image/*"
+          id="image_input"
+          multiple
+          type="file"
+          onChange={(e) => onUpload(e)}
+        />
+        <button
+          type="button"
+          onClick={() => document.getElementById("image_input").click()}
+          className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+        >
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 18"
+          >
+            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+          </svg>
+          <span className="sr-only">Upload image</span>
+        </button>
+        <button
+          type="button"
+          className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+        >
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 16 20"
+          >
+            <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
+          </svg>
+          <span className="sr-only">Set location</span>
+        </button>
+      </div>
+    </div>
   );
 }
