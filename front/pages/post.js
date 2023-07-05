a1963780ca34712b3626826adda69fff7fde826c
suyong choi <dpfprtus@gmail.com>
2023-07-05 22:43:11 +0900

    초안 작성

--- front/pages/post.js
+++ front/pages/post.js
@@ -1,83 +1,115 @@
 import { React, useState, useCallback, useMemo } from "react";
-import dynamic from "next/dynamic";
+
 import "easymde/dist/easymde.min.css";
+import Input from "./../components/Input";
 
-function leftPad(value) {
-  if (value >= 10) {
-    return value;
-  }
-
-  return `0${value}`;
-}
-
-//Client sided에서 동작하는 라이브러리라 ssr을 false로 바꿔 런타임에 동적 import한다.
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
-
-const Edit = (props) => {
+export default function Post() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("initial content");
-  const onChange = useCallback((content) => {
-    setContent(content), [];
-  });
+  const [imageSrc, setImageSrc] = useState(null);
 
-  /* SSR 방식이라 pre-rendering 될때는 window,document객체에 접근할 수없기 때문에
-  localstorage를 이용할 때는 조건문을 사용해야함.
-  */
+  const onChange = (e) => {
+    const { id, value } = e.target;
+    if (id == "title") {
+      setTitle(value);
+    }
+    if (id == "content") {
+      setContent(value);
+    }
+  };
 
-  const autosavedContent =
-    typeof window !== "undefined" ? localStorage.getItem(`smde_demo`) : null;
+  const onUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-  const delay = 2000;
-
-  const anOptions = useMemo(() => {
-    return {
-      autosave: {
-        enabled: true,
-        uniqueId: "editor",
-        delay,
-      },
-    };
-  }, [delay]);
-
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result || null); // 파일의 컨텐츠
+        resolve();
+      };
+    });
+  };
   return (
-    <div>
-      <input
-        className="w-full h-11 mb-2 px-3 border border-[#ced4da] rounded placeholder:font-bold  "
-        placeholder="제목"
-        id="title"
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-      />
-      <SimpleMDE
-        id="editor"
-        value={autosavedContent}
-        placeholder="내용을 입력하세요."
-        onChange={onChange}
-        options={anOptions}
-      />
-
-      <div className="flex justify-center">
-        <button className="px-14 py-1 bg-[#ced4da] rounded-md">취소</button>
-        <button
-          className="px-14 py-1 mx-1 bg-[#f0545459] rounded-md"
-          onClick={() => {
-            const datetime = new Date(Date.now());
-            const time = `${datetime.getFullYear()}-${datetime.getMonth()}-${datetime.getDate()}T${leftPad(
-              datetime.getHours()
-            )}:${leftPad(datetime.getMinutes())}:${leftPad(
-              datetime.getSeconds()
-            )}.000000`;
-            console.log(time);
-          }}
+    <>
+      <div className="mt-10 mx-10 h-max">
+        <div className="mb-6">
+          <Input
+            name="제목"
+            id="title"
+            onChange={onChange}
+            placeholder="제목을 작성하세요"
+          />
+        </div>
+        <label
+          htmlFor="default-input"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          완료
-        </button>
+          내용
+        </label>
+        <div className="r mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+          <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+            <textarea
+              id="content"
+              onChange={onChange}
+              rows="4"
+              className="w-full h-80 px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              placeholder="내용을 작성하세요."
+              required
+            ></textarea>
+            <img width={"50%"} src={imageSrc} />
+          </div>
+          <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+            <button
+              type="submit"
+              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+            >
+              작성하기
+            </button>
+            <div className="flex pl-0 space-x-1 sm:pl-2">
+              <input
+                style={{ height: "0px", overflow: "hidden" }} //사진 아이콘 클릭시 이미지 파일선택창 불러오기 구현
+                accept="image/*"
+                id="image_input"
+                multiple
+                type="file"
+                onChange={(e) => onUpload(e)}
+              />
+              <button
+                type="button"
+                onClick={() => document.getElementById("image_input").click()}
+                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+              >
+                <svg
+                  className="w-4 h-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                >
+                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
+                <span className="sr-only">Upload image</span>
+              </button>
+              <button
+                type="button"
+                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+              >
+                <svg
+                  className="w-4 h-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 16 20"
+                >
+                  <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
+                </svg>
+                <span className="sr-only">Set location</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
-};
-
-export default Edit;
+}
