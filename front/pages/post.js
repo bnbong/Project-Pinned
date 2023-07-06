cea471d868ed1043e9c42fdc2d466ae886b49623
suyong choi <dpfprtus@gmail.com>
2023-07-06 17:16:36 +0900

    추가 로직 필요

--- front/pages/post.js
+++ front/pages/post.js
@@ -1,11 +1,59 @@
 import dynamic from "next/dynamic";
-import { useRef, useState, useMemo } from "react";
+import {
+  useRef,
+  useState,
+  useMemo,
+  useEffect,
+  createRef,
+  useCallback,
+} from "react";
 import Input from "@/components/Input";
 import "react-quill/dist/quill.snow.css";
+import { useMutation } from "react-query";
+import axiosBaseURL from "@/components/axiosBaseUrl";
+import apiMapper from "@/components/apiMapper";
 
 export default function Post({ html, setHtml }) {
-  const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-  const quillRef = useRef(null);
+  const ReactQuill = dynamic(
+    async () => {
+      const { default: RQ } = await import("react-quill");
+      return function comp({ forwardedRef, ...props }) {
+        return <RQ ref={forwardedRef} {...props} />;
+      };
+    },
+    { ssr: false }
+  );
+  const quillRef = useRef(false);
+  const titleInput = useRef();
+  const [title, setTitle] = useState("");
+  const [place, setPlace] = useState("");
+  const [content, setContent] = useState("");
+  //함수컴포넌트는 인스턴트를 가지지않기 때문에 ref객체를 자식 컴포넌트에 넘길 수 없다.
+
+  const imageHandler = () => {
+    const input = document.createElement("input");
+
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    document.body.appendChild(input);
+
+    input.click();
+    input.onchange = async () => {
+      const [file] = input.files;
+
+      // S3 Presigned URL로 업로드하고 image url 받아오기
+      const { preSignedPutUrl: presignedURL, readObjectUrl: imageURL } = (
+        await getS3PresignedURL(file.name)
+      ).data;
+      await uploadImage(presignedURL, file);
+
+      // 현재 커서 위치에 이미지를 삽입하고 커서 위치를 +1 하기
+      const range = quillRef.current.getEditorSelection();
+      quillRef.current.getEditor().insertEmbed(range.index, "image", imageURL);
+      quillRef.current.getEditor().setSelection(range.index + 1);
+      document.body.querySelector(":scope > input").remove();
+    };
+  };
   const modules = useMemo(
     () => ({
       toolbar: {
@@ -16,7 +64,7 @@ export default function Post({ html, setHtml }) {
           [{ list: "ordered" }, { list: "bullet" }, { align: [] }],
           ["image"],
         ],
-        // handlers: { image: imageHandler },
+        handlers: { image: imageHandler },
       },
       clipboard: {
         matchVisual: false,
@@ -39,26 +87,71 @@ export default function Post({ html, setHtml }) {
     "align",
     "image",
   ];
-  const onChange = () => {};
+  const onChange = useCallback(
+    (e) => {
+      if (e.target.id == "title") {
+        setTitle(e.target.value);
+        console.log(title);
+      }
+      if (e.target.id == "place") {
+        setPlace(e.target.value);
+        console.log(place);
+      }
+      if (e.target.id == "content") {
+        setContent(e.target.value);
+      }
+    },
+    [title]
+  );
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      quillRef.current.focus();
+    }
+  };
+  //authorization 토큰 추가 필요
+  const { mutate, data, isLoading, isError } = useMutation({
+    mutationFn: (postInformation) => {
+      return axiosBaseURL.post("api/v1/post/{post_id}", postInformation);
+    },
+    onError: (error, variables, context) => {
+      alert("에러발생");
+    },
+    onSuccess: (data, variables, context) => {
+      console.log("success");
+      alert("게시글이 등록되었습니다.");
+    },
+  });
 
   return (
     <div className="mx-10 mt-10">
       <div className="mb-6">
         <Input
+          name="위치"
+          id="place"
+          onChange={onChange}
+          ref={titleInput}
+          placeholder="제목을 작성하세요"
+        />
+      </div>
+      <div className="mb-6">
+        <Input
           name="제목"
           id="title"
+          onKeyDown={handleEnter}
           onChange={onChange}
+          ref={titleInput}
           placeholder="제목을 작성하세요"
         />
       </div>
       <div>
         <ReactQuill
-          className="h-96"
-          ref={quillRef}
-          onChange={(e) => console.log(e)}
+          className="h-60  rounded-lg"
+          id="content"
+          forwardedRef={quillRef}
+          onChange={onChange}
           modules={modules}
           formats={formats}
-          value={html}
           placeholder={"내용을 작성해 주세요!"}
           theme="snow"
         />
@@ -67,6 +160,7 @@ export default function Post({ html, setHtml }) {
       <div className="flex pl-0 space-x-1 sm:pl-2 mt-20">
         <button
           type="submit"
+          onClick={() => mutate({ title, place, content })}
           className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
         >
           작성하기
