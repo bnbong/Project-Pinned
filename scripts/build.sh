6d37baaee4c45e080f4a1fc9cfba2de7fc512598
bnbong <bbbong9@gmail.com>
2023-07-06 12:03:55 +0900

    [PP-1] swagger 빠진 의존성 추가, 로그 저장 폴더 마운트, 빌드 스크립트 수정

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
-docker compose up --build
+docker compose up --build -d
 docker exec -it "project-pinned-backend-1" python manage.py migrate
 docker exec -it "project-pinned-backend-1" python manage.py landmarkloader media/RB_LANDMARK_INFO_20211231.csv
