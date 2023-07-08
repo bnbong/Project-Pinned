666aee9189ef8551dd5441e4b492b2526bedabef
dpfprtus <dpfprtus@gmail.com>
2023-07-08 17:46:07 +0900

    [PP-15] 무한 스크롤 구현 시작

--- front/HOC/withAuth.js
+++ front/HOC/withAuth.js
@@ -1,44 +1,22 @@
 import { AuthContext } from "@/contexts/AuthContext";
+import { redirect } from "next/dist/server/api-utils";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 
-// 변경 전
-// const withAuth = (Component) => {
-//   const Auth = () => {
-//     // context 등의 상태를 통해 조건부 처리를 한다
-//     const loginState = useContext(AuthContext);
-//     const router = useRouter();
-//     if (!loginState.user) {
-//       router.push("/login");
-//       // Login 컴포넌트를 출력하거나
-//       // 이미 로그인 화면이 구현된 페이지를 사용하고 라우팅
-//       return <>login page</>;
-//     }
-//     // 로그인이 되어있다면
-//     return <Component />;
-//   };
-
-//   return Auth;
-// };
-// export default withAuth;
-
-// 변경 후
 const withAuth = (Component) => {
   const Auth = () => {
     // context 등의 상태를 통해 조건부 처리를 한다
     const loginState = useContext(AuthContext);
     const router = useRouter();
-    useEffect(()=>{
-      if (!loginState.user) {
-        
-        router.push("/login");
-        // Login 컴포넌트를 출력하거나
-        // 이미 로그인 화면이 구현된 페이지를 사용하고 라우팅
-        return <>login page</>;
-      }
-    },[loginState.user,router]);
-    
-    // 로그인이 되어있다면
+
+    if (!loginState.accessToken) {
+      // Login 컴포넌트를 출력하거나
+      // 이미 로그인 화면이 구현된 페이지를 사용하고 라우팅
+      // router.push("/login");
+      alert("로그인이 필요합니다.");
+      router.push("/");
+      return <div>login plz</div>;
+    }
     return <Component />;
   };
 
--- front/pages/index.js
+++ front/pages/index.js
@@ -4,8 +4,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import { getToken, getMessaging } from "firebase/messaging";
 import { messaging } from "@/components/firebase";
-import PostLayout from '@/components/PostLayout';
-import NewPostLayout from '@/components/NewPostLayout';
+import NewPostLayout from "@/components/NewPostLayout";
+import axiosBaseURL from "@/components/axiosBaseUrl";
 
 export default function Home() {
   const [isSupported] = useState(
@@ -17,6 +17,8 @@ export default function Home() {
       : 0
   );
 
+  //page offset
+  const OFFSET = 1;
   const { loginState, setLoginState } = useContext(AuthContext);
 
   // 유저 등록
@@ -119,7 +121,7 @@ export default function Home() {
       // JWT 검증
       const verifyResult = await verifyToken(loginResult.access_token);
       console.log(verifyResult);
-      
+
       let userId = loginResult.user.user_id;
       let userName = loginResult.user.username;
 
@@ -128,7 +130,6 @@ export default function Home() {
       // JWT 새로 발급
       // const refreshResult = await refreshToken(loginResult.refresh_token);
       // console.log(refreshResult);
-      
 
       // 유저 프로필 조회 => 마이페이지에서 사용해야 할듯
       const userProfile = await getUserProfile(
@@ -143,7 +144,7 @@ export default function Home() {
         accessToken: null,
         refreshToken: null,
       });
-      console.log("loginState.user : " +loginState.user);
+      console.log("loginState.user : " + loginState.user);
       console.log("loginState.accessToken" + loginState.accessToken);
       //refreshResult.access
 
@@ -214,73 +215,119 @@ export default function Home() {
       username: "Username1",
       postImage: "https://via.placeholder.com/150",
       likes: 999,
-      description: "Post Description 1..."
+      description: "Post Description 1...",
     },
     {
       profileImage: "profile2.jpg",
       username: "Username2",
       postImage: "https://via.placeholder.com/150",
       likes: 500,
-      description: "Post Description 2..."
+      description: "Post Description 2...",
     },
     {
       profileImage: "profile1.jpg",
       username: "Username1",
       postImage: "https://via.placeholder.com/150",
       likes: 999,
-      description: "Post Description 1..."
-    },{
+      description: "Post Description 1...",
+    },
+    {
       profileImage: "profile1.jpg",
       username: "Username1",
       postImage: "https://via.placeholder.com/150",
       likes: 999,
-      description: "Post Description 1..."
-    },{
+      description: "Post Description 1...",
+    },
+    {
       profileImage: "profile1.jpg",
       username: "Username1",
       postImage: "https://via.placeholder.com/150",
       likes: 999,
-      description: "Post Description 1..."
-    },{
+      description: "Post Description 1...",
+    },
+    {
       profileImage: "profile1.jpg",
       username: "Username1",
       postImage: "https://via.placeholder.com/150",
       likes: 999,
-      description: "Post Description 1..."
-    },{
+      description: "Post Description 1...",
+    },
+    {
       profileImage: "profile1.jpg",
       username: "Username1",
       postImage: "https://via.placeholder.com/150",
       likes: 999,
-      description: "Post Description 1..."
+      description: "Post Description 1...",
     },
   ];
+
+  //post 가져오기
+  const getPost = (param = OFFSET) => {
+    axiosBaseURL
+      .get(`api/v1/post/${param}`)
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // const {
+  //   data, //data.pages를 갖고 있는 배열
+  //   error, // error 객체
+  //   fetchNextPage, // 다음 페이지를 불러오는 함수
+  //   hasNextPage, // 다음 페이지가 있는지 여부, Boolean
+  //   isFetching, // 첫 페이지 fetching 여부, Boolean, 잘 안쓰인다
+  //   isFetchingNextPage, // 추가 페이지 fetching 여부, Boolean
+  //   status, // loading, error, success 중 하나의 상태, string
+  // } = useInfiniteQuery(["post"], getPost, {
+  //   getNextPageParam: (lastPage, page) => {},
+  // });
 
   return (
     <div className="p-5 bg-neutral-50">
       <div className="flex flex-col items-center justify-center mb-5 pb-2.5 bg-neutral-50 bg-opacity-100 shadow-md h-28">
-        <div className='flex justify-between mx-auto'>
+        <div className="flex justify-between mx-auto">
           <div className="flex">
-            <h2 className="text-2xl font-bold m-auto mr-10">이런 장소는 어떠세요?</h2>
+            <h2 className="text-2xl font-bold m-auto mr-10">
+              이런 장소는 어떠세요?
+            </h2>
           </div>
-          <div className='ml-10'>
-            <div className='flex'>
-              <div className='bg-indigo-600 font-semibold rounded-xl p-1 m-1 drop-shadow-md'>
-                <h2 className='text-white'>보정동</h2>
+          <div className="ml-10">
+            <div className="flex">
+              <div className="bg-indigo-600 font-semibold rounded-xl p-1 m-1 drop-shadow-md">
+                <h2 className="text-white">보정동</h2>
               </div>
-              <div className='bg-indigo-600 font-semibold rounded-xl p-1 m-1 drop-shadow-md'>
-                <h2 className='text-white'>행궁동</h2>
+              <div className="bg-indigo-600 font-semibold rounded-xl p-1 m-1 drop-shadow-md">
+                <h2 className="text-white">행궁동</h2>
               </div>
-              <div className='bg-indigo-600 font-semibold rounded-xl p-1 m-1 drop-shadow-md'>
-                <h2 className='text-gray-100'>판교</h2>
+              <div className="bg-indigo-600 font-semibold rounded-xl p-1 m-1 drop-shadow-md">
+                <h2 className="text-gray-100">판교</h2>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {/* {status === "loading" && <p>loading</p>}
+      {status === "error" && <p>{error.message}</p>}
+
+      {status === "success" &&
+        data &&
+        data.pages.map((group, index) => (
+          // pages들이 페이지 숫자에 맞춰서 들어있기 때문에
+          // group을 map으로 한번 더 돌리는 이중 배열 구조이다.
+          // get api를 통해 받은 res를 컴포넌트에 props로 전달해줘서 랜더링해야할거 같음.
+          <div key={index}>
+            {group.results.map((post) => (
+              <p key={post.post_id}>post 컴포넌트가 들어가야한다.</p>
+            ))}
+          </div>
+        ))} */}
+      {/* <button onClick={() => fetchNextPage()}>더 불러오기</button> */}
+
       <button onClick={requestPermission}>FCM</button>
       <div className="grid-cols-1 items-center justify-center">
-        {dummyData.map((post,index) => (
+        {dummyData.map((post, index) => (
           <NewPostLayout
             key={index}
             author={post.username}
@@ -292,5 +339,4 @@ export default function Home() {
       </div>
     </div>
   );
-};
-      
+}
