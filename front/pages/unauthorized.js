b1e574261eff4f0afe4dd170fb9eb7d1edc9a7a6
dpfprtus <dpfprtus@gmail.com>
2023-07-10 00:43:16 +0900

    [PP-20] FIX localstorage

--- front/pages/unauthorized.js
+++ front/pages/unauthorized.js
@@ -9,7 +9,7 @@ export default function Unauthorized() {
     <section>
       <h1>권한없음</h1>
       <br />
-      <p>로그인이 필요합니다..</p>
+      <p>로그인이 필요합니다...</p>
       <div className="flexGrow">
         <button onClick={goBack}>뒤로가기</button>
       </div>
