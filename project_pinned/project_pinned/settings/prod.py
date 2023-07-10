73e3f4dc58ccbf9dbf320a48615727fc778ec6d2
bnbong <bbbong9@gmail.com>
2023-07-10 17:10:51 +0900

    S3 location 수정

--- project_pinned/project_pinned/settings/prod.py
+++ project_pinned/project_pinned/settings/prod.py
@@ -49,7 +49,7 @@ CACHES = {
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_REGION = 'ap-northeast-2'
+AWS_REGION = 'ap-southeast-2'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
