8dcb4703a92a12cc975d5894a30ca738cef77b61
JaeoneHeo <jaewonheo666@gmail.com>
2023-07-12 00:22:46 +0900

    [PP-17] 로그아웃 로직 구현

---
+++ front/pages/tset.js
@@ -0,0 +1,210 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import axiosBaseURL from "@/components/axiosBaseUrl";
+import React, { useState, useEffect } from 'react';
+
+
+export default function NavBar() {
+  const [activeTab, setActiveTab] = useState('trending');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axiosBaseURL.get('api/v1/post/feed');
+  
+      if (activeTab === 'followed') {
+        setPosts(response.data.followed_posts);
+      } else if (activeTab === 'trending') {
+        setPosts(response.data.trending_posts);
+      } else if (activeTab === 'recommended') {
+        setPosts(response.data.recommended_posts);
+      }
+    }
+  
+    fetchData();
+  }, [activeTab]); 
+
+  return (
+    <>
+      <div className="fixed bottom-0 z-50 w-full -translate-x-1/2 bg-white border-t border-gray-200 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+        <div className="w-full">
+          <div
+            className="grid max-w-xs grid-cols-3 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-gray-600"
+            role="group"
+          >
+            <button
+              type="button"
+              className="px-5 py-1.5 text-xs font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg"
+              onClick={() => setActiveTab('followed')}
+            >
+              Following
+            </button>
+            <button
+              type="button"
+              className="px-5 py-1.5 text-xs font-medium text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900 rounded-lg"
+              onClick={() => setActiveTab('trending')}
+            >
+              Trending
+            </button>
+            <button
+              type="button"
+              className="px-5 py-1.5 text-xs font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg"
+              onClick={() => setActiveTab('recommended')}
+            >
+              Recommended
+            </button>
+          </div>
+        </div>
+        <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
+          <Link href="/">
+            <button
+              data-tooltip-target="tooltip-home"
+              type="button"
+              className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            >
+              <svg
+                className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+              </svg>
+              <span className="sr-only">Home</span>
+            </button>
+          </Link>
+          <div
+            id="tooltip-home"
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Home
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+          <Link href="/map">
+            <button
+              data-tooltip-target="tooltip-bookmark"
+              type="button"
+              className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            >
+              <svg
+                className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
+              </svg>
+              <span className="sr-only">Map</span>
+            </button>
+          </Link>
+          <div
+            id="tooltip-bookmark"
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Map
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              data-tooltip-target="tooltip-new"
+              type="button"
+              className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+            >
+              <Link href="/post">
+                <svg
+                  className="w-6 h-6 text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  ></path>
+                </svg>
+              </Link>
+              <span className="sr-only">New post</span>
+            </button>
+          </div>
+          <div
+            id="tooltip-new"
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Create new post
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+          <button
+            data-tooltip-target="tooltip-search"
+            type="button"
+            className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <Link href="/search">
+              <svg
+                className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                ></path>
+              </svg>
+              <span className="sr-only">Search</span>
+            </Link>
+          </button>
+          <div
+            id="tooltip-search"
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Search
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+
+          <button
+            data-tooltip-target="tooltip-profile"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <Link href="/mypage">
+              <svg
+                className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                ></path>
+              </svg>
+            </Link>
+            <span className="sr-only">Profile</span>
+          </button>
+
+          <div
+            id="tooltip-profile"
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Profile
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
