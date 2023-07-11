3f2787ab6a93b48d475648cb61741073e6bde5b3
JaeoneHeo <jaewonheo666@gmail.com>
2023-07-11 19:21:57 +0900

    [PP-18] Search 개발 완료 & 유저 페이지 개발 완료 (팔로우 제외)

--- front/pages/user/[id].js
+++ front/pages/user/[id].js
@@ -3,19 +3,19 @@ import { AuthContext } from "@/contexts/AuthContext";
 import React, { useContext, useEffect, useState } from "react";
 import { useRouter } from 'next/router';
 import axiosBaseURL from '@/components/axiosBaseUrl';
-
+import NewPostLayout from '@/components/PostLayout';
 export default function UserPage () {
   //이 uesr 는 사용자 자신을 의미. 일단 내비두기.
   const { user } = useContext(AuthContext);
 
   //현재 페이지는 다른 유저의 페이지.
-  const router =useRouter();
+  const router = useRouter();
   if (!router.isReady) return null;
-  let {id} = router.query;
+  const { id } = router.query;  // 바로 userID로 사용
 
-  const [userID, setUserID] = useState(id);
+  const [userID, setUserID] = useState('');
   const [active, setActive] = useState(false);
-  const [userName, setUserName] = useState(null);
+  const [userName, setUserName] = useState("로딩중...");
   const [follower, setFollower] = useState(0);
   const [following, setFollowing] = useState(0);
   const [postNumber, setPostNumber] = useState(0); 
@@ -41,53 +41,60 @@ export default function UserPage () {
 
   
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setActive(!active);
     //팔로우 했을때 안했을때 나눠서 적용.
     if(active === true){
-      const followNow = async () => {
-        try{
-          const res = await axiosBaseURL.post( `api/v1/user/${userID}/follow/`);
-        }catch(error) {
-          console.log(error);
-        }     
-      }
+      try{
+        console.log("팔로우 시작");
+        const res = await axiosBaseURL.post( `api/v1/user/${userID}/follow/`);
+        console.log(res);
+      } catch(error) {
+        console.log(error);
+      }     
     } else {
-      const unfollow = async () => {
-        try{
-          const res = await axiosBaseURL.post(`api/v1/user/${userID}/unfollow/`);
-        } catch(error) {
-            console.log(error);
-        }
+      try{
+        console.log("팔로우 해제");
+        const res = await axiosBaseURL.post(`api/v1/user/${userID}/unfollow/`);
+        console.log(res);
+      } catch(error) {
+          console.log(error);
       }
     }
   }
 
   useEffect(() => {
+    setUserID(router.query.id);
+  }, [id]);
+
+  useEffect(() => {
+    
     const fetchPosts = async () => {
+      if (userID == 'undefined' || userID == null) return;
+      console.log(userID);
       try {
+        console.log("포스트 불러오기");
         const resPost = await axiosBaseURL.get(`api/v1/post/posts/${userID}/`)
-        .then((resPost)=>{
-          //유저 Post 설정 + 이해하기 쉽게 resPost라 명명
-          setPosts(resPost.data.user_posts);
-        })
+        //유저 Post 설정
+        setPosts(resPost.data.user_posts);
+
+        console.log("프로필 불러오기");
         const resProfile = await axiosBaseURL.get(`api/v1/user/${userID}/profile/`)
-        .then((resProfile)=>{
-          //유저 이름, 팔로워, 팔로잉 설정 + 이해하기 쉽게 resProfile라
-          setUserName(resProfile.data.username);
-          setFollower(resProfile.data.followers);
-          setFollowing(resProfile.data.followings);
-        })
+        //유저 이름, 팔로워, 팔로잉 설정
+        setUserName(resProfile.data.username);
+        setFollower(resProfile.data.followers);
+        setFollowing(resProfile.data.followings);
       } catch (error) {
         console.log(error);
       }
     };
-  }, []);
+    fetchPosts();
+  }, [userID]);
 
   useEffect(()=>{
     setPostNumber(posts.length); 
   },[posts]);
-
+  
   return (
     <div className='p-5 bg-neutral-50'>
       <div className='flex flex-col items-center justify-center mb-5 pb-2.5 bg-neutral-50 bg-opacity-100 shadow-md'>
@@ -120,14 +127,12 @@ export default function UserPage () {
         </div>
       </div>
       <div className="grid-cols-1 items-center justify-center">
-        {dummyData.map((post,index) => (
-          <PostLayout
-            key={index}
-            profileImage= {post.profileImage}
-            username= {post.username} 
-            postImage={post.postImage} 
-            likes={post.likes} 
-            description={post.description}
+        {posts.map((post,index) => (
+          <NewPostLayout
+          author={post.username}
+          location={post.landmark_name}
+          title={post.post_title}
+          content={post.post_content}
           />
         ))}
       </div>
