ba174f3b048cee0b969b2b3c7adc3ddd21c8aa55
johnhuh619 <jaewonheo666@gmail.com>
2023-09-02 20:21:50 +0900

    [PP-27] 프로필 이미지 업로드 기능 구현. dev에서 렌더링 2회 되는 현상 해제

--- front/components/axiosBaseUrl.js
+++ front/components/axiosBaseUrl.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import toast from "react-hot-toast";
-import mem from "mem";
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import mem from 'mem';
 
 /**
  * refresh api 요청이 중복되서 서버에 전송되면 DB에 저장된 refresh 토큰과 다른 토큰을 클라이언트가
@@ -14,48 +14,55 @@ const getNewAccessToken = mem(
     try {
       const {
         data: { access },
-      } = await axiosBaseURL.post("api/v1/user/token/refresh/");
+      } = await axiosBaseURL.post('api/v1/user/token/refresh/');
 
       return access;
     } catch (err) {
-      toast.error("다시 로그인해주세요.");
-      localStorage.removeItem("access_token");
+      toast.error('다시 로그인해주세요.');
+      localStorage.removeItem('access_token');
     }
   },
   { maxAge: 1000 }
 );
 
 const axiosBaseURL = axios.create({
-  baseURL: "https://mypinnedlandmark.bnbong.tk:8000/", // 프로덕션 이미지 빌드 시 실제 URL로 변경
+  baseURL: 'https://mypinnedlandmark.bnbong.tk/', // 프로덕션 이미지 빌드 시 실제 URL로 변경
   withCredentials: true,
 });
 
 const accessToken =
-  typeof window !== "undefined" ? localStorage.getItem("access_token") : null;
+  typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
 
 axiosBaseURL.interceptors.request.use(
   (config) => {
-    console.log("requeset interceptor", config.url);
+    console.log('requeset interceptor', config.url);
     // 모든 Request Header에 Access토큰을 넣어주는 역할
-    if (!config.headers["Authorization"] && accessToken !== "" && accessToken) {
+    if (!config.headers['Authorization'] && accessToken !== '' && accessToken) {
       //메인 피드 동적으로 변경시 변경 필요
-      if (config.url === "api/v1/post/feed") {
-        config.headers["Authorization"] = "";
-        config.headers["Content-Type"] = "application/json";
+      if (config.url === 'api/v1/post/feed') {
+        config.headers['Authorization'] = '';
+        config.headers['Content-Type'] = 'application/json';
       } else {
-        config.headers["Authorization"] = `Bearer ${accessToken}`;
-        config.headers["Content-Type"] = "application/json";
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+        config.headers['Content-Type'] = 'application/json';
       }
-      return config;
+      //return config;
     }
     //login & signup 페이지에선 access_token을 header에서 없앤다.
     if (
-      config.url === "api/v1/user/login/" ||
-      config.url === "api/v1/user/register/" ||
-      config.url === "api/v1/landmark/landmarks"
+      config.url === 'api/v1/user/login/' ||
+      config.url === 'api/v1/user/register/' ||
+      config.url === 'api/v1/landmark/landmarks'
     ) {
-      config.headers["Authorization"] = "";
-      config.headers["Content-Type"] = "application/json";
+      config.headers['Authorization'] = '';
+      config.headers['Content-Type'] = 'application/json';
+    }
+    if (
+      config.url.includes('api/v1/user/') &&
+      config.url.includes('profile/') &&
+      config.method == 'put'
+    ) {
+      config.headers['Content-Type'] = 'multipart/form-data';
     }
     return config;
   },
@@ -67,24 +74,24 @@ axiosBaseURL.interceptors.response.use(
   async (error) => {
     const prevRequest = error?.config;
 
-    //mypage api에선 로그아웃 연장이 안된다.
-    if (error?.response?.status === 401 && !prevRequest?.sent) {
-      console.log("token");
-      const accessToken = await getNewAccessToken()
-        .then((res) => {
-          localStorage.removeItem("access_token");
-          localStorage.setItem("access_token", res);
-        })
-        .catch((err) => {
-          toast.error("다시 로그인 해주세요!");
-          throw err;
-        });
+    //mypage api에선 로그아웃 연장이 안된다. =>> 이 부분 때문에 재발급 계속 받음.
+    // if (error?.response?.status === 401 && !prevRequest?.sent) {
+    //   console.log('401 token 재발급');
+    //   const accessToken = await getNewAccessToken()
+    //     .then((res) => {
+    //       localStorage.removeItem('access_token');
+    //       localStorage.setItem('access_token', res);
+    //     })
+    //     .catch((err) => {
+    //       toast.error('다시 로그인 해주세요!');
+    //       throw err;
+    //     });
 
-      prevRequest.sent = true;
-      prevRequest.headers["Authorization"] = `Bearer ${accessToken}`;
-    }
+    //   prevRequest.sent = true;
+    //   prevRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+    // }
     if (error?.response?.status >= 500) {
-      toast.error("서버 에러가 발생했습니다.");
+      toast.error('서버 에러가 발생했습니다.');
     }
     return Promise.reject(error);
   }
